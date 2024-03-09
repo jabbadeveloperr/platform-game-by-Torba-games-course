c2001f3cafbb0e82ba66162119d2c2379dd04abb
Vitalii Lytvynov <jabbadeveloper@gmail.com>
2024-03-09 04:27:35 +0200

    add background

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
